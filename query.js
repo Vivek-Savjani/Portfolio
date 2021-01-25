0ab43538f62b1281525aec7ed6b461ffa7178b72
Vivek Savjani <viveknsavjani@gmail.com>
2021-01-25 20:20:07 +0000

    JSON outputing to js array

--- query.js
+++ query.js
@@ -1,10 +1,18 @@
 
 $(document).ready(function(){
     $("button").click(function(){
+        var output = [];
+        var test = 1;
       $.getJSON("https://api.github.com/users/Vivek-Savjani/repos", function(result){
         $.each(result, function(i, field){
-          console.log(field)
+          output.push(field);
         });
+        console.log(output[0]);
+        console.log(output[1]);
+        console.log(output[2]);
       });
+      
     });
   });
+
+
