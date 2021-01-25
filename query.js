6c19e1ae2a6797dcd2b427ca410a332d6055e955
Vivek Savjani <viveknsavjani@gmail.com>
2021-01-25 20:54:52 +0000

    objects accesible as objects in js

--- query.js
+++ query.js
@@ -2,7 +2,8 @@
 $(document).ready(function(){
     $("button").click(function(){
         var output = [];
-        var test = 1;
+        var testarray = [,];
+        var list = 
       $.getJSON("https://api.github.com/users/Vivek-Savjani/repos", function(result){
         $.each(result, function(i, field){
           output.push(field);
@@ -10,6 +11,9 @@ $(document).ready(function(){
         console.log(output[0]);
         console.log(output[1]);
         console.log(output[2]);
+        document.write(output[1])
+        output.forEach(element => testarray.push(output.indexOf(element),output[output.indexOf(element)].description));
+        testarray.forEach(element => console.log(element));
       });
       
     });
