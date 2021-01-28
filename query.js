5ca48e666355d21519677727e5793d5d9068e14f
Vivek Savjani <viveknsavjani@gmail.com>
2021-01-28 15:15:10 +0000

    dynamic grid

--- query.js
+++ query.js
@@ -1,22 +1,23 @@
-let i =0
+var cpr = 3;
+let i =0;
+console.log("Cards per row should be:"+cpr);
 $.getJSON("https://api.github.com/users/Vivek-Savjani/repos", function(data){
         var repo_data = '';
         
         $.each(data, function(key, value){
           var url = "http://raw.githubusercontent.com/" +  value.full_name + "/main/images/preview.png";
-          if (i == 0) {repo_data += '<div class="wrapper">'}
+          //if (i == 0) {repo_data += '<div class="wrapper">'}
           repo_data += '<div class = "projcard"> ';
           repo_data += '<img src="'+url +' " onerror="this.src=`alt.png`">';
           repo_data += '<div class = "info">';
-          repo_data += '<h2>'+value.full_name+'</h2>';
+          repo_data += '<h2>'+value.name+'</h2>';
           repo_data += ' <p>Description: '+value.description;
           repo_data += '</br>Language: '+ value.language+'</p>';
-          repo_data += '<button onclick=href"'+value.html_url+'">Go to project page</button>';
+          repo_data += '<button onclick="window.location.href=`'+value.html_url+'`;" >Go to project page</button>';
           repo_data += '</div></div>';
           console.log(i);
-
             i +=1;
-            if (i == 3) {repo_data += "</div>", i += -3}
+            //if (i == cpr) {repo_data += "</div>", i += -cpr}
             
         });
         $('#repo_table').append(repo_data);
@@ -31,18 +32,18 @@ $.getJSON("https://api.github.com/users/Vivek-Savjani/repos", function(data){
         var alt = "'alt.png'"
         console.log(url);
         if (bool == true) {
-          if (i == 0) {repo_data += '<div class="wrapper">'}
+          //if (i == 0) {repo_data += '<div class="wrapper">'}
             repo_data += '<div class = "projcard"> ';
             repo_data += '<img src="'+url +' " onerror="this.src=`alt.png`">';
             repo_data += '<div class = "info">';
-            repo_data += '<h2>'+value.full_name+'</h2>';
+            repo_data += '<h2>'+value.name+'</h2>';
             repo_data += ' <p>Description: '+value.description;
             repo_data += '</br>Language: '+ value.language+'</p>';
-            repo_data += '<button onclick=href"'+value.html_url+'">Go to project page</button>';
+            repo_data += '<button onclick="window.location.href=`'+value.html_url+'`;" >Go to project page</button>';
             repo_data += '</div></div>';
-            console.log(i);
+            console.log(value.html_url);
             i +=1;
-            if (i == 3) {repo_data += "</div>", i += -3}
+            //if (i == cpr) {repo_data += "</div>", i += -cpr}
            
         }
         });
@@ -50,6 +51,5 @@ $.getJSON("https://api.github.com/users/Vivek-Savjani/repos", function(data){
  });
   
 
- var target = 'https://github.com/CSCoursework/Battleships';
     
 function checksubstring(string,substring)  {return (string.indexOf(substring) !== -1)}
