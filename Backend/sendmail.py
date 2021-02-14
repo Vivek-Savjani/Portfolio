5cb2589ee6184720265f05371d70961953779d26
Vivek Savjani <viveknsavjani@gmail.com>
2021-02-14 22:30:43 +0000

    Now running code runs from non-static server

--- Backend/sendmail.py
+++ Backend/sendmail.py
@@ -1,8 +1,21 @@
+from flask import Flask,request,render_template
+app = Flask(__name__)
+
+@app.route('/addr', methods=['POST','GET'])
+def addr():
+    return render_template("Vivek.html")
+
+
 import smtplib
 mail = smtplib.SMTP(host = "smtp.gmail.com")
 mail.starttls()
 password = ""
 email = ""
 mail.login(email,password)
-mail.sendmail(email,email,"test")
+mail.sendmail(email,email,"test7")
 mail.close()
+
+
+##set FLASK_APP=sendmail.py
+##py -m flask run
+##set FLASK_ENV=development
