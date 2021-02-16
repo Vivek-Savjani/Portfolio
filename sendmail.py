0ec7633fe1e22c77d3e92b1d3f0e337f2777c494
Vivek Savjani <viveknsavjani@gmail.com>
2021-02-16 14:37:01 +0000

    Running when called by form

---
+++ sendmail.py
@@ -0,0 +1,23 @@
+from flask import Flask,request,render_template
+app = Flask(__name__)
+
+@app.route('/data', methods=['POST','GET'])
+def data():
+    print("works here")
+    import smtplib
+    mail = smtplib.SMTP(host = "smtp.gmail.com")
+    mail.starttls()
+    password = ""
+    email = ""
+    mail.login(email,password)
+    mail.sendmail(email,email,"test18")
+    mail.close()
+    return "test19"
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port='8000', debug=True)
+
+
+##set FLASK_APP=sendmail.py
+##py -m flask run
+##set FLASK_ENV=development
