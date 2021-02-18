a7dceae9278cc402e259972ee9d5eb8e26492778
Vivek Savjani <viveknsavjani@gmail.com>
2021-02-18 19:21:27 +0000

    Removed test directory

--- test/navbar.js
+++
@@ -1,8 +0,0 @@
-function navbar() {
-  var x = document.getElementById("navitems");
-  if (x.style.display === "block") {
-    x.style.display = "none";
-  } else {
-    x.style.display = "block";
-  }
-}
